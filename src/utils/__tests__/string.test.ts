4d9475d851ee941d95e98b73b502f063e20497b6
LoreviQ <lorevi@protonmail.com>
2025-04-12 19:33:20 +0100

    added jest config and string utility tests

---
+++ src/utils/__tests__/string.test.ts
@@ -0,0 +1,24 @@
+import { wrapInCodeBlock, joinWithNewlines } from '../string';
+
+describe('string utils', () => {
+    describe('wrapInCodeBlock', () => {
+        it('should wrap a string in json code block', () => {
+            const input = '{\"key\": \"value\"}';
+            const expected = '```json\n{\"key\": \"value\"}\n```';
+            expect(wrapInCodeBlock(input)).toEqual(expected);
+        });
+    });
+
+    describe('joinWithNewlines', () => {
+        it('should join strings with double newlines, filtering undefined', () => {
+            const input = ['first', undefined, 'second', 'third'];
+            const expected = 'first\n\nsecond\n\nthird';
+            expect(joinWithNewlines(input)).toEqual(expected);
+        });
+
+        it('should return an empty string for empty or all undefined input', () => {
+            expect(joinWithNewlines([])).toEqual('');
+            expect(joinWithNewlines([undefined, undefined])).toEqual('');
+        });
+    });
+}); 
